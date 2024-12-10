f033d163be27f55a98e7d0846a490b8eb6fc6d47
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-12-10 18:25:39 +0200

    working Rap

--- topic_10_object_oriented_programming/song_rap.py
+++ topic_10_object_oriented_programming/song_rap.py
@@ -52,3 +52,36 @@ song2 = Song("Bohemian Rhapsody", "Queen", ["Is this the real life?", "Is this j
 song1.sing().yell(2)
 
 song2.yell(3).sing(2)
+
+# now let's create a rap song class that will inherit from Song class	
+
+class Rap(Song):
+    # custom __init__ method is not needed if we do not need to change the way the object is created
+    # def __init__(self, title="", author="", lyrics=()):
+    #     # Mantojam no Song klases
+    #     super().__init__(title, author, lyrics)
+    #    print(f"New Rap Song made: {self.title} - {self.author}")  # Pārdefinējam __init__ metodi
+ 
+    def break_it(self, max_lines=-1, drop="yeah"):
+        """
+        Izdrukā dziesmu tekstu, pievienojot 'drop' vārdu pēc katra vārda.
+        """
+        self._print_title_author() # if we had used __print_title_author() it would not be accessible here
+        # we then would need to unmangle the name _Song__print_title_author()
+        
+        if max_lines == -1:  # Ja nav ierobežots rindiņu skaits, izdrukā visas
+            for line in self.lyrics:
+                # Pievienojam 'drop' vārdu aiz katra vārda
+                print(' '.join([f"{word} {drop}" for word in line.split()]))
+        else:
+            for i in range(min(max_lines, len(self.lyrics))):
+                # Pievienojam 'drop' vārdu aiz katra vārda
+                print(' '.join([f"{word} {drop}" for word in self.lyrics[i].split()]))
+        
+        return self  # Ķēdēšanai
+    
+song3 = Rap("Shape of You", "Ed Sheeran", ["The club isn't the best place to find a lover", "So the bar is where I go", "Me and my friends at the table doing shots", "Drinking fast and then we talk slow"])
+song4 = Rap("Hotel California", "Eagles", ["On a dark desert highway, cool wind in my hair", "Warm smell of colitas, rising through the air", "Up ahead in the distance, I saw a shimmering light", "My head grew heavy and my sight grew dim"])
+
+song3.break_it(2, "yo").sing(1)
+song4.break_it(3, "ahem").yell(2)
