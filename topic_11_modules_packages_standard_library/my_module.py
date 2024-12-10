e8822b63dcf8622e8c4c5306a1665824543c2d9c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-12-10 18:55:49 +0200

    modules and importing

--- topic_11_modules_packages_standard_library/my_module.py
+++ topic_11_modules_packages_standard_library/my_module.py
@@ -14,7 +14,15 @@ MY_PI = 3.1415926 # by convention constants are written in all caps, but not enf
 # functions in modules should not depend on anything besides inputs and should not change anything outside of the function
 # some function
 
+# good practice would be to have docstrings for functions that you write for importing by others
+# also add type hints whenever they make sense
+
 def print_my_shopping_list(my_list):
+    """
+    This function prints a shopping list
+    :param my_list: list of items
+    :return: None
+    """
     print("I need to buy:")
     for item in my_list:
         print(item)
@@ -22,10 +30,20 @@ def print_my_shopping_list(my_list):
 
 # another function
 def my_adder(a, b):
+    """	
+    This function adds two numbers
+    :param a: number
+    :param b: number
+    :return: sum of a and b
+    """
     return a + b
 
 # a class for pet
 class Pet:
+    """
+    This class represents a pet
+    We could write more about it here
+    """
     def __init__(self, name, species):
         self.name = name
         self.species = species
@@ -34,19 +52,25 @@ class Pet:
         return f"{self.name} is a {self.species}"
     
     # make a sound
-    def make_sound(self, sound):
+    def get_sound(self, sound:str) -> str:
+        """
+        This method makes a sound
+        :param sound: str
+        :return: str
+        """
         return f"{self.name} says {sound}"
 
 
 # let's use __main__ to run some code when the module is run
 # now I can use this for a script or for testing my module
+# I could do nothing as well if I do not want to run anything
 if __name__ == "__main__":
     print("My module is active!")
     print("This will only run when the module is run directly")
     my_cat = Pet("Fluffy", "cat")
     my_dog = Pet("Rex", "dog")
-    print(my_cat.make_sound("meow"))
-    print(my_dog.make_sound("woof"))
+    print(my_cat.get_sound("meow"))
+    print(my_dog.get_sound("woof"))
     print_my_shopping_list(my_shopping_list)
     # i could add some tests here
     # this is a good practice to have tests in your module
--- topic_11_modules_packages_standard_library/my_program.py
+++ topic_11_modules_packages_standard_library/my_program.py
@@ -13,3 +13,36 @@ import my_module # think of it as a giant copy paste of the file my_module.py
 # so those files that we plan to use as modules we want to have some code that will only run when the file is run directly
 
 print("My program is running")
+
+# since we imported my_module we can use anything that is defined in that file
+# we have gained so called namespace
+# so we avoid polluting the global namespace
+
+# i can print MY_PI from my_module
+print("Value of PI from my_module:", my_module.MY_PI)
+
+my_module.print_my_shopping_list(my_module.my_shopping_list)
+
+# i could have my own shopping list
+another_list = ["milk", "bread", "eggs"]
+my_module.print_my_shopping_list(another_list)
+
+my_zebra = my_module.Pet("Marty", "zebra")
+print(my_zebra)
+print(my_zebra.get_sound("neigh"))
+
+# i could make more pets now
+my_fish = my_module.Pet("Nemo", "fish")
+print(my_fish)
+print(my_fish.get_sound("blub"))
+
+# so why use modules?
+# - to organize code
+# - to reuse code
+# - to avoid name conflicts
+# - to make code easier to read
+
+# downsides
+# - can be slower to import - usually not an issue
+# - can be harder to debug - usually not an issue
+# - can be harder to understand - usually you should not need to understand the whole module
