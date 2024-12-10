3e99765b802b80ed1543aafc3226782fb95f3485
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-12-10 18:19:41 +0200

    Song solution

--- topic_10_object_oriented_programming/song_rap.py
+++ topic_10_object_oriented_programming/song_rap.py
@@ -14,14 +14,17 @@ class Song:
         self._print_title_author()
         
         if max_lines == -1:  # Ja nav ierobežots rindiņu skaits, izdrukā visas
-            for line in self.lyrics:
-                print(line)
-        else:
-            # for i in range(min(max_lines, len(self.lyrics))):
-            #     print(self.lyrics[i])
-            # no need to use index we can use slice which is more Pythonic
-            for line in self.lyrics[:max_lines]: # remember that slice will not give us error if max_lines is bigger than the list
-                print(line)
+            # for line in self.lyrics:
+            #     print(line)
+            # we can set max_lines to be the length of the list
+            max_lines = len(self.lyrics)
+        # this way we avoid the use of else
+        # for i in range(min(max_lines, len(self.lyrics))):
+        #     print(self.lyrics[i])
+        # no need to use index we can use slice which is more Pythonic
+        for line in self.lyrics[:max_lines]: # remember that slice will not give us error if max_lines is bigger than the list
+            print(line)
+            # we could add a call to some Text-to-Speech library here
         
         return self  # Ķēdēšanai
  
@@ -45,5 +48,7 @@ song1 = Song("Imagine", "John Lennon", ["Imagine there's no heaven", "It's easy 
 
 song2 = Song("Bohemian Rhapsody", "Queen", ["Is this the real life?", "Is this just fantasy?", "Caught in a landslide", "No escape from reality"])
 
-# we have two song objects
+# we have two song objects created from same Song class - template
 song1.sing().yell(2)
+
+song2.yell(3).sing(2)
