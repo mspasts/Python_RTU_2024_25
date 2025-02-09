1a37ec79df8e91907e2a95c2210a1f8f58320ad5
M-Stirna <mspasts@proton.me>
2025-02-09 15:14:31 +0200

    dienas_uzdevums2.1

---
+++ topic_2_data_types_input/dienas_uzdevums2.1.py
@@ -0,0 +1,18 @@
+"""
+1. Uzdevums
+Uzrakstiet programmu, kas noprasa un saglabā lietotāja vārdu
+Uzdod jautājumu par lietotāja vecumu, jautājumā izmantojot lietotāja vārdu.
+Uzrāda pēc cik gadiem lietotājam būs 100 gadi :)
+"""
+
+print("Ši programma noprasa un saglabā lietotāja vārdu")
+name = input("What is your name, friend? ")
+print(f"Name {name} saved")
+age = input(f"What is your age , {name}? ")
+age = float(age)
+age_to_100 = 100 - age
+rounded_age = round(age_to_100)
+print (f"You will be 100 years old in {rounded_age} years")
+birthyear = input("What year were you born, {name}?")
+hundred_years_old = float(birthyear) + 100
+print (f"You will be 100 years old in year {hundred_years_old}")
